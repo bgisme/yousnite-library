2c9be7b1da96ae8d1a1e4c07e938cd8353d0f080
bgisme <55024704+bgisme@users.noreply.github.com>
2024-06-21 07:45:53 -0400

    Tweaks

--- Sources/Utilities/Protocols/Validatable.swift
+++ Sources/Utilities/Protocols/Validatable.swift
@@ -29,84 +29,56 @@ extension Validatable where Self == Value {
 
 extension Bool: Validatable {
     public typealias Value = Self
-//    
-//    public var description: String { self ? "true" : "false" }
 }
 
 extension String: Validatable {
     public typealias Value = Self
-//    
-//    public var stringValue: String? { self }
 }
 
 extension Double: Validatable {
     public typealias Value = Self
-//    
-//    public var stringValue: String? { description }
 }
 
 extension Float: Validatable {
     public typealias Value = Self
-//    
-//    public var stringValue: String? { self.formatted() }
 }
 
 extension Int: Validatable {
     public typealias Value = Self
-    
-//    public var stringValue: String? { self.formatted() }
 }
 
 extension Int8: Validatable {
     public typealias Value = Self
-    
-//    public var stringValue: String? { self.formatted() }
 }
 
 extension Int16: Validatable {
     public typealias Value = Self
-    
-//    public var stringValue: String? { self.formatted() }
 }
 
 extension Int32: Validatable {
     public typealias Value = Self
-    
-//    public var stringValue: String? { self.formatted() }
 }
 
 extension Int64: Validatable {
     public typealias Value = Self
-    
-//    public var stringValue: String? { self.formatted() }
 }
 
 extension UInt: Validatable {
     public typealias Value = Self
-    
-//    public var stringValue: String? { self.formatted() }
 }
 
 extension UInt8: Validatable {
     public typealias Value = Self
-    
-//    public var stringValue: String? { self.formatted() }
 }
 
 extension UInt16: Validatable {
     public typealias Value = Self
-    
-//    public var stringValue: String? { self.formatted() }
 }
 
 extension UInt32: Validatable {
     public typealias Value = Self
-    
-//    public var stringValue: String? { self.formatted() }
 }
 
 extension UInt64: Validatable {
     public typealias Value = Self
-    
-//    public var stringValue: String? { self.formatted() }
 }
